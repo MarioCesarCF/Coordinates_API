28f114c07076254c040c927255890eb0cf4ac0b6
Mario Cesar <mc.fialho1@gmail.com>
2025-01-17 22:01:09 -0300

    refactor: adiciona controle de cache

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
+import cors from "cors";
 import "dotenv/config";
 import express from "express";
 import connection from "./config/dbConfig.js";
-import cors from "cors";
 import router from "./routes/index.route.js";
 
 const app = express();
 app.use(cors());
 connection();
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    res.setHeader('Surrogate-Control', 'no-store');
+    next();
+});
 
 app.use(router);
 
